21d0116a35dc2f8e25552b1cd94b0ff3bc930b70
karenm08 <wmok14@my.bcit.ca>
2021-04-11 09:27:13 -0700

    refresh Auth token  and uselocal storage for auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,25 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Dashboard from './layouts/Dashboard';
 import Home from './layouts/Home';
 import GuardedRoute from './components/GuardedRoute';
+import { refreshAuthToken } from './userAuth';
+import useLocalStorage from 'react-use-localstorage';
+import { Redirect } from 'react-router-dom';
 
 export default function App() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useLocalStorage(
+        'isAuthorized',
+        false
+    );
 
-    const authenticateUser = (authState) => {
-        setIsAuthenticated(authState);
-    };
+    useEffect(() => {
+        (async () => {
+            try {
+                await refreshAuthToken(setIsAuthenticated);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, []);
 
     useEffect(() => {
         console.log(`Authenticated: ${isAuthenticated}`);
@@ -17,19 +29,13 @@ export default function App() {
 
     return (
         <Router>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">Home Page</Link>
-                    </li>
-                    <li>
-                        <Link to="/dashboard">Dashboard Page</Link>
-                    </li>
-                </ul>
-            </div>
             <Switch>
                 <Route exact path="/">
-                    <Home setIsAuthenticated={setIsAuthenticated} />
+                    {localStorage.getItem('isAuthorized') ? (
+                        <Redirect from="/" to="/dashboard" />
+                    ) : (
+                        <Home setIsAuthenticated={setIsAuthenticated} />
+                    )}
                 </Route>
                 <GuardedRoute
                     path="/dashboard"
