9dbf69d5aa1da214e9cdca8973bfb959cd0048e2
Steven <clai86@my.bcit.ca>
2021-04-12 15:20:46 -0700

    Deployment backend to heroku successfully

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Amplify from 'aws-amplify';
-import config from './config';
+import config from './config.json';
 
 Amplify.configure({
     Auth: {
