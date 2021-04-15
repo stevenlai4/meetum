203953c7b2c1835a6f335282ccc3da3ca11425cc
karenm08 <wmok14@my.bcit.ca>
2021-04-15 12:24:22 -0700

    style invitation button

--- frontend/src/components/DashboardPage/InvitationCard.js
+++ frontend/src/components/DashboardPage/InvitationCard.js
@@ -1,39 +1,56 @@
-import { Card, Typography } from '@material-ui/core';
+import { Card, Typography, Button, Input } from '@material-ui/core';
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import { Redirect, useHistory } from 'react-router-dom';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Moment from 'react-moment';
+import { green, red } from '@material-ui/core/colors';
 
 export default function InvitationCard({ invitation }) {
     const classes = useStyles();
     const history = useHistory();
 
     const handleCardOnClick = () => {
-        history.push(`/invitation/${invitation._id}`);
+        alert('see more');
+        // history.push(`/invitation/${invitation._id}`);
     };
 
     return (
-        <Card className={classes.card} onClick={handleCardOnClick}>
-            <Typography className={classes.name} variant="h5">
-                {invitation?.event_id?.name}
-            </Typography>
-            <Moment className={classes.date} format="YYYY/MM/DD">
-                {invitation?.event_id?.date}
-            </Moment>
-            <Moment className={classes.date} format="hh:mm:ss">
-                {invitation?.event_id?.date}
-            </Moment>
-            <div className={classes.createdOnContainer}>
-                <Typography className={classes.createdOnText} component="p">
-                    Create On:
-                </Typography>{' '}
-                <Moment className={classes.createdOnText}>
-                    {invitation.createdOn}
+        <Card className={classes.card}>
+            <div>
+                <Typography className={classes.name} variant="h5">
+                    {invitation?.event_id?.name}
+                </Typography>
+                <Moment className={classes.date} format="YYYY/MM/DD">
+                    {invitation?.event_id?.date}
                 </Moment>
+                <Moment className={classes.date} format="hh:mm:ss">
+                    {invitation?.event_id?.date}
+                </Moment>
+                <div className={classes.createdOnContainer}>
+                    <Typography className={classes.createdOnText} component="p">
+                        Create On:
+                    </Typography>{' '}
+                    <Moment className={classes.createdOnText}>
+                        {invitation.createdOn}
+                    </Moment>
+                </div>
             </div>
-            <Typography className={classes.location} component="p">
-                {invitation.location}
-            </Typography>
+            <div className={classes.buttonContainer}>
+                <div className={classes.acceptContainer}>
+                    <input
+                        className={classes.input}
+                        placeholder="Add your location"
+                        type="text"
+                        // value={participantEmails}
+                        autoComplete="on"
+                        // onChange={(e) => setParticipantEmails(e.target.value)}
+                    ></input>
+                    <AcceptButton> Accept</AcceptButton>
+                </div>
+                <div className={classes.declineContainer}>
+                    <DeclineButton>Decline</DeclineButton>
+                </div>
+            </div>
         </Card>
     );
 }
@@ -46,12 +63,21 @@ const useStyles = makeStyles((theme) => ({
         margin: '20px auto',
         padding: '10px 10px 50px 10px',
         position: 'relative',
-        cursor: 'pointer',
         borderRadius: 10,
-        transition: 'ease-in-out 0.6s',
-        '&:hover': {
-            background: 'rgba(255,255,255,0.4)',
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    input: {
+        width: '60%',
+        // marginLeft: '20px',
+        padding: '3%',
+        border: 'none',
+        borderRadius: '10px',
+        '&:focus': {
+            outline: 'none',
         },
+        background: 'rgba(255,255,255,0.2)',
+        color: 'black',
     },
     name: {
         textTransform: 'capitalize',
@@ -71,7 +97,39 @@ const useStyles = makeStyles((theme) => ({
         bottom: 5,
         left: 10,
     },
+    buttonContainer: {
+        width: '40%',
+    },
+    acceptContainer: { display: 'flex' },
+    declineContainer: { display: 'flex', justifyContent: 'flex-end' },
     createdOnText: {
         fontSize: 12,
     },
 }));
+
+//style for decline button
+const DeclineButton = withStyles((theme) => ({
+    root: {
+        color: theme.palette.getContrastText(red[500]),
+        backgroundColor: red[500],
+        '&:hover': {
+            backgroundColor: red[700],
+        },
+        marginTop: '20px',
+        width: '37%',
+    },
+}))(Button);
+
+//style for accept button
+const AcceptButton = withStyles((theme) => ({
+    root: {
+        color: theme.palette.getContrastText(green[500]),
+        backgroundColor: green[500],
+        '&:hover': {
+            backgroundColor: green[700],
+        },
+        marginLeft: '10px',
+        color: '#FFF',
+        width: '40%',
+    },
+}))(Button);
